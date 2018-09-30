f456ebd5dc83b7dc68e0d1013bd1b8a5d1fd9549
José Henrique da Silva guimarães <42553922+JHenriqueSG@users.noreply.github.com>
2018-09-30 07:54:02 -0300

    Initial commit

---
+++ ContaPoupança.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolePoupança
+{
+
+    class ContaPoupança
+    {
+        static double taxaDeJuroAnual = 0.04;
+        double saldoPoupança;
+
+
+        public ContaPoupança() { }
+      
+        public ContaPoupança(double saldoPoupança)
+        {
+            this.saldoPoupança = saldoPoupança;
+
+
+        }
+        public double calcularJuroMensal()
+        {
+            this.saldoPoupança = (((taxaDeJuroAnual * saldoPoupança) / 12) + saldoPoupança);
+            return this.saldoPoupança;
+        }
+
+        public static void alterarTaxaDeJuros(double taxaDeJuros)
+        {
+            ContaPoupança.taxaDeJuroAnual = taxaDeJuros;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsolePoupança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaPoupança poupador1 = new ContaPoupança(2000.00);
+            ContaPoupança poupador2 = new ContaPoupança(3000.00);
+
+            Console.WriteLine("poupador1, 1°mes");
+            Console.WriteLine(poupador1.calcularJuroMensal());
+            Console.WriteLine("poupador2, 1°mes");
+            Console.WriteLine(poupador2.calcularJuroMensal());
+            ContaPoupança.alterarTaxaDeJuros(0.05);
+            Console.WriteLine("poupador1, 2°mes");
+            Console.WriteLine(poupador1.calcularJuroMensal());
+            Console.WriteLine("poupador2, 2°mes");
+            Console.WriteLine(poupador2.calcularJuroMensal());
+
+
+
+
+            Console.WriteLine("\n\n\nPressione qualquer tecla para sair ...");
+            Console.ReadKey(true);
+        }
+    }
+}
